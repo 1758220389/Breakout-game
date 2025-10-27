d08c8f83a177af4454a1888f58a835c354c61988
lllllvlv <gokyph@gamil.com>
2025-10-27 23:39:57 +0800

    特殊砖块

--- specialbrick.js
+++ specialbrick.js
@@ -15,9 +15,9 @@ var speeddown = 12;//减速
 var tankball = 13;//穿透因子
 var routate = 14;//旋转因子
 
-function brickeffect(brick)
+function brickeffect(brick, c, r, brickarray, brickColumnCount, brickRowCount, gameState)
 {
-    switch(brick.specialbrick, c, r, bricks, brickColumnCount, brickRowCount, gameState)
+    switch(brick.specialbrick)
     {
         case normalbrick:
             brick.status = 0;
@@ -38,7 +38,6 @@ function brickeffect(brick)
 
                 brick.status = 0;
             }
-            brick.status = 0;
             break;
         case vectorbrickdown:
             if(gameState.ballLastDY < 0)//球向上运动表示球从上方击中
@@ -61,10 +60,17 @@ function brickeffect(brick)
         case boombrick:
             brick.status = 0;
             // 摧毁周围砖块
-            for(var i = Math.max(0, c-1); i <= Math.min(brickColumnCount-1, c+1); i++) {
-                for(var j = Math.max(0, r-1); j <= Math.min(brickRowCount-1, r+1); j++) {
-                    if(bricks[i][j].status > 0) {
-                        bricks[i][j].status = 0;
+             for(var i = Math.max(0, c-1); i <= Math.min(brickColumnCount-1, c+1); i++) 
+            {
+                for(var j = Math.max(0, r-1); j <= Math.min(brickRowCount-1, r+1); j++) 
+                {
+                    // 跳过自身
+                    if(i === c && j === r) continue;
+                    
+                    // 确保砖块存在且状态>=1
+                    if(brickarray[i] && brickarray[i][j] && brickarray[i][j].status >= 1) 
+                    {
+                        brickarray[i][j].status = 0;
                         gameState.score++; // 为每个被摧毁的砖块加分
                     }
                 }
@@ -73,9 +79,10 @@ function brickeffect(brick)
         case chainbrick:
             brick.status = 0;
             // 摧毁整行砖块
-            for(var i = 0; i < brickColumnCount; i++) {
-                if(bricks[i][r].status > 0) {
-                    bricks[i][r].status = 0;
+            for(var i = 0; i < brickColumnCount; i++) 
+            {
+                if(brickarray[i] && brickarray[i][r] && brickarray[i][r].status >= 1) {
+                    brickarray[i][r].status = 0;
                     if(i !== c) gameState.score++; // 避免重复计分
                 }
             }
@@ -83,14 +90,15 @@ function brickeffect(brick)
         case motherbrick:
             brick.status = 0;
             // 在周围生成新砖块
-            var directions = [[0,1], [1,0], [0,-1], [-1,0]]; // 上下左右
-            for(var d = 0; d < directions.length; d++) {
+             var directions = [[0,1], [1,0], [0,-1], [-1,0]]; // 上下左右
+            for(var d = 0; d < directions.length; d++) 
+            {
                 var newC = c + directions[d][0];
                 var newR = r + directions[d][1];
                 if(newC >= 0 && newC < brickColumnCount && newR >= 0 && newR < brickRowCount) {
-                    if(bricks[newC][newR].status === 0) {
-                        bricks[newC][newR].status = 1;
-                        bricks[newC][newR].specialbrick = normalbrick; // 新生成的为普通砖块
+                    if(brickarray[newC] && brickarray[newC][newR] && brickarray[newC][newR].status === 0) {
+                        brickarray[newC][newR].status = 1;
+                        brickarray[newC][newR].specialbrick = normalbrick; // 新生成的为普通砖块
                     }
                 }
             }
